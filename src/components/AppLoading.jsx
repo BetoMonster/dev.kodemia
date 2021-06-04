289409a482cf2c6b917026c0f522ee84c82eb33b
Beto Barrera <betomonter@gmail.com>
2021-06-03 20:45:54 -0500

    add useEffect

--- src/components/AppLoading.jsx
+++ src/components/AppLoading.jsx
@@ -1,5 +1,5 @@
 //import React from 'react';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./appLoading.css";
 
 /*class AppLoading extends React.Component{
